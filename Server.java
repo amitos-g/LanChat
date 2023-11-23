eb198270881872ee57af05611c8890ae616ffedc
amitos-g <amitginat15@gmail.com>
2023-11-23 14:57:41 +0200

    :w

--- Server.java
+++ Server.java
@@ -3,21 +3,27 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class ClientHandler{
+class ClientHandler implements Runnable{
     private String name;
     private Socket socket;
     private DataOutputStream dataOutputStream;
     private DataInputStream dataInputStream;
     
 
-    public ClientHandler(String name, Socket socket) {
+    public ClientHandler(Socket socket) {
       this.socket = socket;
       try{
       this.dataInputStream = new DataInputStream(socket.getInputStream());
       this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
-      }
+      //try to get the name of the client
+      this.name = this.dataInputStream.readUTF();
+      //if got the name:
+      this.dataOutputStream.writeUTF("Welcome To The Server %s".formatted(this.name));
+        
+        
+    }
       catch (Exception e){
-          this.socket.send
+          
       }
     }
 
@@ -37,7 +43,37 @@ class ClientHandler{
     public void setSocket(Socket socket) {
       this.socket = socket;
     }
+    
+    public DataOutputStream getDataOutputStream() {
+      return dataOutputStream;
+    }
 
+    public void setDataOutputStream(DataOutputStream dataOutputStream) {
+      this.dataOutputStream = dataOutputStream;
+    }
+
+    public DataInputStream getDataInputStream() {
+      return dataInputStream;
+    }
+
+    public void setDataInputStream(DataInputStream dataInputStream) {
+      this.dataInputStream = dataInputStream;
+    }
+
+    @Override
+    public void run(){
+      //listen for messages and send them to the server.
+      while(true){
+        try{
+        String message = this.dataInputStream.readUTF();
+        Server.sendToServer(this.formatMessage(message));
+        }
+        catch(IOException e){
+
+        }
+      }
+      
+    }
 
     
 
@@ -47,28 +83,12 @@ class ClientHandler{
 public class Server {
 
   private static ArrayList<ClientHandler> connections = new ArrayList<>();
-  private static ServerSocket serverSocket;
+  private static ServerSocket server;
   public static void main(String[] args) {
     try {
       System.out.println("this is the server. listening on port 2006.");
-      serverSocket = new ServerSocket(2006);
-      //listen for people
-      Socket connectedSocket = server.accept();
-      DataInputStream inputStream = new DataInputStream(connectedSocket.getInputStream());
-      String entered = inputStream.readUTF();
-      System.out.println("Client sent the path -> " + entered);
-      entered = entered.replace("\\", "/");
-      //"entered" is the file's path. lets read it.
-      String content = readFile(entered);
-
-      //lets send the content back.
-      DataOutputStream outputStream = new DataOutputStream(connectedSocket.getOutputStream());
-      outputStream.writeUTF(content);
-      outputStream.flush();
-      System.out.println("Sent the client the content of the file at that path");
-      
-      
-      server.close();
+      server = new ServerSocket(2006);
+      acceptAndConnect();
 
     } catch (Exception e) {
       e.printStackTrace();
@@ -82,9 +102,12 @@ public class Server {
           public void run(){
              while(true){
               try{  
-              Socket newConnection = Server.serverSocket.accept();
-              //get his name
-              String name = newConnection.
+              Socket newConnection = Server.server.accept();
+              //try to create a socket of him
+              ClientHandler newClient = new ClientHandler(newConnection);
+              connections.add(newClient);
+              Thread clienThread = new Thread(newClient);
+              clienThread.start();
             }
               catch(IOException ioException){
                  
@@ -92,25 +115,18 @@ public class Server {
              }
           }
       };
-  }
-
-  private static String readFile(String path){
-    try{
-    File file = new File(path);
-    Scanner reader = new Scanner(file);
-
-    StringBuilder builder = new StringBuilder();
-
-    while(reader.hasNextLine()){
-        builder.append(reader.nextLine() + "\n");
+      Thread acceptorThread = new Thread(acceptor);
+      acceptorThread.start();
     }
-    return builder.toString();
-  }
 
-    catch(Exception e){
-      e.printStackTrace();
-    }
-  return null;
-  }
+  public static void sendToServer(String message){
+      for(var client : connections){
+        try{
+          client.getDataOutputStream().writeUTF(message);
+        }
+        catch(Exception e){
 
+        }
+      }
+  }
 }
